348880186ada493381697ee8927c4f74ac120192
HyeokChan <qpjiq123@gmail.com>
2021-12-19 23:05:16 +0900

    init test project

---
+++ src/main/java/me/chan/test/Study.java
@@ -0,0 +1,4 @@
+package me.chan.test;
+
+public class Study {
+}
---
+++ src/test/java/me/chan/test/StudyTest.java
@@ -0,0 +1,14 @@
+package me.chan.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudyTest {
+    @Test
+    void crate() {
+        Study study = new Study();
+    }
+
+
+}
